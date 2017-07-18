3c095d0a405155b2b760e5d5d045fa50c701a859
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-07-19 00:10:31 +0900

    add request router

--- routes/request.js
+++ routes/request.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-// DBへの接続
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/equipment_management_system');
 
-// モデルの宣言
 var Request = require('../app/models/request');
 var User = require('../app/models/user');
 var Equipment = require('../app/models/equipment');
 
-router.get('/api/request', function(req, res, next) {
+router.get('/api/request', function (req, res, next) {
     Request.find()
         .sort({
             'timestamp': -1
@@ -24,18 +22,52 @@ router.get('/api/request', function(req, res, next) {
         });
 });
 
-router.post('/api/request', function(req, res, next) {
-    Request.find()
-        .sort({
-            'timestamp': -1
-        })
-        .populate('user')
-        .populate('equipment')
-        .exec(function (err, requests) {
-            if (err)
-                res.send(err);
-            res.json(requests);
+router.post('/api/request', function (req, res, next) {
+    var user;
+    var equipment;
+
+    User.findOne({
+        name: req.user_name
+    }).exec(function (err, existUser) {
+        if (err)
+            res.send(err);
+        if (existUser !== "" || existUser === null) {
+            user = new User();
+            user.name = req.user_name;
+        } else
+            user = existUser;
+    });
+
+    Equipment.findOne({
+        name: req.equipment_name
+    }).exec(function (err, existEquip) {
+        if (err)
+            res.send(err);
+        if (existEquip !== "" || existEquip === null) {
+            equipment = new Equipment();
+            user.name = req.equipment_name;
+        } else
+            equipment = existEquipment;
+    });
+
+    var request = new Request();
+    var d = new Date();
+
+    request.user = user;
+    request.equipment = equipment;
+    request.quantity = req.quantity;
+    request.remarks = req.remarks;
+    request.url = req.url;
+    request.timestamp = ISODate(d.toISOString + 'T00+09:00');;
+    request.status = 1;
+
+    request.save(function (err) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'New Request created!'
         });
+    });
 });
 
 module.exports = router;
