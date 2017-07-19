ed0a74670d8e30c35d6f89c156a74e5fda17729b
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-07-19 17:39:44 +0900

    modify

--- routes/request.js
+++ routes/request.js
@@ -5,8 +5,6 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/equipment_management_system');
 
 var Request = require('../app/models/request');
-var User = require('../app/models/user');
-var Equipment = require('../app/models/equipment');
 
 router.get('/api/request', function (req, res, next) {
     Request.find()
