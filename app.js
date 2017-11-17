08c390a97140f8fda91d4a6732195475f4b00396
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-11-17 22:07:19 +0900

    temp save

--- app.js
+++ app.js
@@ -210,6 +210,26 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 /**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
  * Event listener for HTTP server "error" event.
  */
 
