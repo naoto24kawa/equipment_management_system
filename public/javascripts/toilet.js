4ce9f64d361f260226f46859d4f717722bb84d66
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-11-17 19:41:24 +0900

    temp save

--- public/javascripts/toilet.js
+++ public/javascripts/toilet.js
@@ -1,6 +1,7 @@
 $(function () {
     var socket = io();
     socket.on(`toilet`, (status) => {
+        console.log('receive toilet status ' + status);
         if(status == 1) {
             $('#toilet_status').attr('href', 'images/empty.png');
         } else {
