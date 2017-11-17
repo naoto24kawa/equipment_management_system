e9bac948fc2a387d37278b890d50920a964e760a
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-11-17 22:17:57 +0900

    temp save

--- public/javascripts/toilet.js
+++ public/javascripts/toilet.js
@@ -3,10 +3,10 @@ $(function () {
     socket.on(`toilet`, (status) => {
         $('#favicon').remove();
         if(status == 0) {
-            $('head').append('<link type="image/x-icon" id="favicon" rel="icon" href="favicon_empty.png">');
+            $('head').append('<link type="image/x-icon" id="favicon" rel="icon" href="images/favicon_empty.png">');
             $('#toilet_status').attr('src', 'images/empty.png');
         } else {
-            $('head').append('<link type="image/x-icon" id="favicon" rel="icon" href="favicon_using.png">');
+            $('head').append('<link type="image/x-icon" id="favicon" rel="icon" href="images/favicon_using.png">');
             $('#toilet_status').attr('src', 'images/using.png');
         }
     });
