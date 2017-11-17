bbbf9dcf459978d04ec4f5cb43d50aadc41ba5f4
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-11-17 22:01:38 +0900

    temp save

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ toilet_router.post('/api', function (req, res, next) {
             });
         });
         // これやりたいけど難しそう
-        // io.emit(`toilet`, toilet_model.status);
+        io.emit(`toilet`, toilet_model.status);
     } else {
         res.json({
             message: 'not found toilet status.'
@@ -174,4 +174,80 @@ app.use(function (err, req, res, next) {
     });
 });
 
-module.exports = app;
+// ==============================
+
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Atouch socket.io.
+ */
+var socketio = require('socket.io')(server);
+
+socketio.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ?
+        'pipe ' + addr :
+        'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+//module.exports = app;
+
+// ==============================
