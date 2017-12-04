14517b0965a05ed3e5d83f063f11ada477f75a83
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-12-04 19:40:06 +0900

    temp save

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-//app.use(express.static('public/plise_propaganda_2017/html'));
+app.use(express.static('public/plise_propaganda_2017/html'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
