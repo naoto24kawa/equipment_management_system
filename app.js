e4a6e493ebbdfb2fc72f6830d6a99a5f855c5f27
西川 直登 <mg_nishikawa_naoto@MG.LOCAL>
2017-12-04 19:14:41 +0900

    temp save

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var flash = require("connect-flash");
-var session = require("express-session");
 
 var app = express();
 
@@ -14,11 +9,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
 
 app.use('/', express.static('public/plise_propaganda_2017/html/index.html'));
 
