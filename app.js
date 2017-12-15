f9c3521997fac8d8fd2d210b6bf9ed774efe010f
nishikawa_naoto <s1210202@gmail.com>
2017-12-15 15:48:22 +0900

    temp save

--- app.js
+++ app.js
@@ -1,3 +1,100 @@
+// ==============================
+
+var http = require('http');
+var debug = require('debug')('equipment-management-system:server');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Atouch socket.io.
+ */
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	var bind = typeof port === 'string' ?
+		'Pipe ' + port :
+	'Port ' + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === 'string' ?
+		'pipe ' + addr :
+	'port ' + addr.port;
+	debug('Listening on ' + bind);
+}
+
+// ==============================
+
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -11,68 +108,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 var request = require('./routes/request');
-//var toilet = require('./routes/toilet');
-
-// ==============================
-
-var Toilet = require('./app/models/toilet')
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_qb55r46q:5mhum2i8dqoqhp8k4h8nuulqfv@ds015690.mlab.com:15690/heroku_qb55r46q');
-
-var toilet_router = express.Router();
-
-toilet_router.get('/', function (req, res, next) {
-    Toilet.find()
-        .sort({
-            'timestamp': -1
-        })
-        .exec(function (err, toilets) {
-            if (err)
-                res.send(err);
-            res.render('toilet', {
-                status: toilets[0].status
-            });
-        });
-});
-
-toilet_router.get('/api', function (req, res, next) {
-    Toilet.find()
-        .sort({
-            'timestamp': -1
-        })
-        .exec(function (err, toilets) {
-            if (err)
-                res.send(err);
-            res.json(toilets);
-        });
-});
-
-toilet_router.post('/api', function (req, res, next) {
-
-    var toilet_model = new Toilet();
-
-    if (req.body.status != null) {
-        toilet_model.status = req.body.status;
-        toilet_model.timestamp = Date.now();
-
-        toilet_model.save(function (err) {
-            if (err)
-                res.send(err);
-            res.json({
-                message: 'success create toilet status.'
-            });
-        });
-        // これやりたいけど難しそう
-        socketio.emit(`toilet`, toilet_model.status);
-    } else {
-        res.json({
-            message: 'not found toilet status.'
-        });
-    }
-});
-
-// ==============================
+var toilet = require('./routes/toilet')(io);
 
 var User = require('./app/models/user');
 
@@ -150,7 +186,7 @@ app.use(passport.session());
 
 app.use('/', index);
 app.use('/request', request);
-app.use('/toilet', toilet_router);
+app.use('/toilet', toilet);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -174,101 +210,4 @@ app.use(function (err, req, res, next) {
     });
 });
 
-// ==============================
-
-var http = require('http');
-var debug = require('debug')('equipment-management-system:server');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Atouch socket.io.
- */
-var socketio = require('socket.io')(server);
-
-socketio.on('connection', (socket) => {
-    console.log('a user connected');
-});
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string' ?
-        'Pipe ' + port :
-        'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ?
-        'pipe ' + addr :
-        'port ' + addr.port;
-    debug('Listening on ' + bind);
-}
-
 //module.exports = app;
-
-// ==============================
--- routes/toilet.js
+++ routes/toilet.js
@@ -6,54 +6,57 @@ mongoose.connect('mongodb://heroku_qb55r46q:5mhum2i8dqoqhp8k4h8nuulqfv@ds015690.
 
 var Toilet = require('../app/models/toilet');
 
-//router.get('/', function (req, res, next) {
-//    Toilet.find()
-//        .sort({
-//            'timestamp': -1
-//        })
-//        .exec(function (err, toilets) {
-//            if (err)
-//                res.send(err);
-//            res.render('toilet', {
-//                status: toilets[0].status
-//            });
-//        });
-//});
+/* GET home page. */
+var returnRouter = function(io) {
+	router.get('/', function (req, res, next) {
+		Toilet.find()
+			.sort({
+			'timestamp': -1
+		})
+			.exec(function (err, toilets) {
+			if (err)
+				res.send(err);
+			res.render('toilet', {
+				status: toilets[0].status
+			});
+		});
+	});
 
-//router.get('/api', function (req, res, next) {
-//    Toilet.find()
-//        .sort({
-//            'timestamp': -1
-//        })
-//        .exec(function (err, toilets) {
-//            if (err)
-//                res.send(err);
-//            res.json(toilets);
-//        });
-//});
+	router.get('/api', function (req, res, next) {
+		Toilet.find()
+			.sort({
+			'timestamp': -1
+		})
+			.exec(function (err, toilets) {
+			if (err)
+				res.send(err);
+			res.json(toilets);
+		});
+	});
 
-//router.post('/api', function (req, res, next) {
-//
-//    var toilet = new Toilet();
-//
-//    if (req.body.status != null) {
-//        toilet.status = req.body.status;
-//        toilet.timestamp = Date.now();
-//
-//        toilet.save(function (err) {
-//            if (err)
-//                res.send(err);
-//            res.json({
-//                message: 'success create toilet status.'
-//            });
-//        });
-//        // これやりたいけど難しそう
-//        // io.emit(`toilet`, toilet.status);
-//    } else {
-//        res.json({
-//            message: 'not found toilet status.'
-//        });
-//    }
-//});
+	router.post('/api', function (req, res, next) {
 
-module.exports = router;
+		var toilet = new Toilet();
+
+		if (req.body.status != null) {
+			toilet.status = req.body.status;
+			toilet.timestamp = Date.now();
+
+			toilet.save(function (err) {
+				if (err)
+					res.send(err);
+				res.json({
+					message: 'success create toilet status.'
+				});
+			});
+			// これやりたいけど難しそう
+			 io.emit(`toilet`, toilet.status);
+		} else {
+			res.json({
+				message: 'not found toilet status.'
+			});
+		}
+	});
+}
+
+module.exports = returnRouter;
